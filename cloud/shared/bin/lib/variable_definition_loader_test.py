7688ea915c4086bc003ae19af1c63e59202bbe5c
Jessica Hummel <jhummel@google.com>
2023-03-03 15:28:16 +0000

    Add comments and fix bug in test

--- cloud/shared/bin/lib/variable_definition_loader_test.py
+++ cloud/shared/bin/lib/variable_definition_loader_test.py
@@ -3,22 +3,31 @@ import json
 import os
 
 from cloud.shared.bin.lib.variable_definition_loader import VariableDefinitionLoader
+"""
+ Tests for the VariableDefinitionLoader. Tests the loading of configuration
+ variables from a json file.
+
+ To run the tests from the root direcory of the deploy-infra repostory run:
+    PYTHONPATH="${PYTHONPATH}:${pwd}" 
+    python3 cloud/shared/bin/lib/variable_definition_loader_test.py
+"""
 
 class TestVariableDefinitionLoader(unittest.TestCase):
+
     __json_file_path_name = "./test_variable_definition_file.json"
 
     __valid_variable_defs = {
-            "FOO": {
+            "BING": {
                 "required": True,
                 "secret": False,
                 "type": "string"
             },
-            "Bar": {
+            "BONG": {
                 "required": True,
                 "secret": False,
                 "type": "string"
             },
-            "Bat": {
+            "BOB": {
                 "required": False,
                 "secret": False,
                 "type": "string"
@@ -26,38 +35,42 @@ class TestVariableDefinitionLoader(unittest.TestCase):
         }
     
     __variable_defs_with_duplicate = {
-        "Pen": {
+        "BING": {
             "required": True,
             "secret": False,
             "type": "string"
         },
-        "Bar": {
+        "BONG": {
             "required": True,
             "secret": False,
             "type": "string"
         },
-        "Baf": {
+        "BONG": {
             "required": False,
             "secret": False,
             "type": "string"
         },
     }
 
-    def test_validate_all_variable_definitions(self):        
-        varDefLoader = VariableDefinitionLoader()
+
+    def test_load_valid_variable_definitions(self):   
+        self.__json_file_path_name = "./valid_vars_testfile.json"
+        varDefLoader = VariableDefinitionLoader({})
         self._write_json_file(self.__valid_variable_defs, self.__json_file_path_name)
         varDefLoader.load_definition_file(self.__json_file_path_name)
-        vars = varDefLoader.get_variable_definitions() 
+        vars = varDefLoader.get_variable_definitions()
         self.assertEqual(vars, self.__valid_variable_defs)
 
-    # def test_duplicate_var_throws_error(self):
-        varDefLoader = VariableDefinitionLoader()
-        self._write_json_file(self.__valid_variable_defs, self.__json_file_path_name)
+    def test_duplicate_variable_throws_error(self):
+        self.__json_file_path_name = "./invalid_vars_testfile.json"
+        varDefLoader = VariableDefinitionLoader({})
+        self._write_json_file(self.__variable_defs_with_duplicate, self.__json_file_path_name)
         try:
             varDefLoader.load_definition_file(self.__json_file_path_name)
         except RuntimeError as e:
-            self.assertEqual(e.args[0], "Duplicate variable name: FOO at ./test_variable_definition_file.json")
-        
+            self.assertEqual(e.args[0], "Duplicate variable name: BONG at ./test_variable_definition_file.json") 
+
+     
     
     def _write_json_file(self, json_content, filepath: str):        
         defs_string = json.dumps(json_content)
@@ -65,10 +78,7 @@ class TestVariableDefinitionLoader(unittest.TestCase):
             var_defs_file.write(defs_string)
 
     def tearDown(self):
-
-        os.remove("./test_variable_definition_file.json")
-
-          
+        os.remove(self.__json_file_path_name)
 
 if __name__ == '__main__':
     unittest.main()
