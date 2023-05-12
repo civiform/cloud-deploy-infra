8cbd3677eb5fd62b427bc3c73aa2b818a1b70c36
Reema <7339672+reemaka@users.noreply.github.com>
2023-05-12 14:35:54 -0400

    Update cloud/aws/templates/aws_oidc/variables.tf
    Co-authored-by: rocky <rachelefine@gmail.com>

--- cloud/aws/templates/aws_oidc/variables.tf
+++ cloud/aws/templates/aws_oidc/variables.tf
@@ -422,7 +422,8 @@ variable "esri_find_address_candidate_url" {
   description = "The URL CiviForm will use to call Esri’s findAddressCandidates service."
   default     = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates"
 }
-variable "PUBLISH_SINGLE_PROGRAM_ENABLED" {
+
+variable "publish_single_program_enabled" {
   type        = bool
   description = "Whether to enable the feature that allows publishing a single program on its own."
   default     = false
