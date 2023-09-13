fa0ec748dbc7beec6f7b1fef6e41010c391344b8
dkatzz <86739416+dkatzz@users.noreply.github.com>
2023-09-13 15:34:23 -0400

    Allow RDS performance insights and advanced monitoring to be enabled (#258)

--- cloud/aws/templates/aws_oidc/main.tf
+++ cloud/aws/templates/aws_oidc/main.tf
@@ -53,6 +53,9 @@ resource "aws_db_instance" "civiform" {
   kms_key_id                      = aws_kms_key.civiform_kms_key.arn
   storage_encrypted               = true
   enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+  performance_insights_enabled    = var.rds_performance_insights_enabled
+  monitoring_role_arn             = var.rds_enhanced_monitoring_enabled ? aws_iam_role.civiform_enhanced_monitoring_role[0].arn : null
+  monitoring_interval             = var.rds_enhanced_monitoring_enabled ? var.rds_enhanced_monitoring_interval : null
 }
 
 # Provide database information for other resources (pgadmin, for example).
@@ -61,6 +64,31 @@ data "aws_db_instance" "civiform" {
   depends_on = [
     aws_db_instance.civiform
   ]
+}
+
+# IAM Role for RDS Enhanced Monitoring
+resource "aws_iam_role" "civiform_enhanced_monitoring_role" {
+  count = var.rds_enhanced_monitoring_enabled ? 1 : 0
+  name  = "civiform_enhanced_monitoring_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "monitoring.rds.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"]
+
+  tags = {
+    Name = "civiform_enhanced_monitoring_role"
+  }
 }
 
 module "email_service" {
