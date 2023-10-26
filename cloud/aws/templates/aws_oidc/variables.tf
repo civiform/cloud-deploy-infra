623d427bcc99be8111682cd1c859c423f4c6b79e
Rocky Fine <rocky.fine@exygy.com>
2023-10-26 16:34:10 -0400

    remove STAGING_ADD_NOINDEX_META_TAG

--- cloud/aws/templates/aws_oidc/variables.tf
+++ cloud/aws/templates/aws_oidc/variables.tf
@@ -490,11 +490,6 @@ variable "show_civiform_image_tag_on_landing_page" {
   description = "Whether to show civiform version on landing page or not."
   default     = false
 }
-variable "staging_add_noindex_meta_tag" {
-  type        = bool
-  description = "Whether to add a robots=noindex meta tag, which causes search engines to not list the website. This defaults to false, meaning sites will appear on search."
-  default     = false
-}
 variable "staging_disable_demo_mode_logins" {
   type        = bool
   description = "Whether to disable the demo mode login buttons."
