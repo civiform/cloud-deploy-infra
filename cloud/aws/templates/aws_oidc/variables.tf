462420130dd353ed987749be3805e0a5d12117d6
dkatzz <86739416+dkatzz@users.noreply.github.com>
2023-12-18 20:34:06 -0500

    Update variables.tf

--- cloud/aws/templates/aws_oidc/variables.tf
+++ cloud/aws/templates/aws_oidc/variables.tf
@@ -335,38 +335,42 @@ variable "ecs_server_container_memory" {
   type        = number
   description = "The amount (in MiB) of memory to present to the server container."
   default     = 4096
-  validation {
-    condition = var.ecs_server_container_memory + var.ecs_metrics_scraper_container_memory <= var.ecs_task_memory
-    error_message = "The ECS_SERVER_CONTAINER_MEMORY + ECS_METRICS_SCRAPER_CONTAINER_MEMORY must be less than or equal to the ECS_TASK_MEMORY"
-  }
 }
 
 variable "ecs_server_container_memory_reservation" {
   type        = number
   description = "The soft limit (in MiB) of memory to reserve for the server container."
   default     = 2048
-  validation {
-    condition = var.ecs_server_container_memory > var.ecs_server_container_memory_reservation
-    error_message = "ECS_SERVER_CONTAINER_MEMORY_RESERVATION must be less than the ECS_SERVER_CONTAINER_MEMORY"
-  }
 }
 
 variable "ecs_metrics_scraper_container_memory" {
   type        = number
   description = "The amount (in MiB) of memory to present to the metrics scraper container."
   default     = 2048
-  validation {
-    condition = var.ecs_server_container_memory + var.ecs_metrics_scraper_container_memory <= var.ecs_task_memory
-    error_message = "The ECS_SERVER_CONTAINER_MEMORY + ECS_METRICS_SCRAPER_CONTAINER_MEMORY must be less than or equal to the ECS_TASK_MEMORY"
-  }
 }
 
 variable "ecs_metrics_scraper_container_memory_reservation" {
   type        = number
   description = "The soft limit (in MiB) of memory to reserve for the metrics scraper container."
   default     = 1024
-  validation {
-    condition = var.ecs_metrics_scraper_container_memory > var.ecs_metrics_scraper_container_memory_reservation
+}
+
+# This is a workaround for validation until terraform supports conditions referring to other variables (https://github.com/hashicorp/terraform/issues/25609)
+locals {
+  server_memory_validation = {
+    condition     = var.ecs_server_container_memory > var.ecs_server_container_memory_reservation
+    error_message = "ECS_SERVER_CONTAINER_MEMORY_RESERVATION must be less than the ECS_SERVER_CONTAINER_MEMORY"
+  }
+  server_memory_res_validation = {
+    condition     = var.ecs_server_container_memory > var.ecs_server_container_memory_reservation
+    error_message = "ECS_SERVER_CONTAINER_MEMORY_RESERVATION must be less than the ECS_SERVER_CONTAINER_MEMORY"
+  }
+  metrics_scraper_memory_validation = {
+    condition     = var.ecs_server_container_memory + var.ecs_metrics_scraper_container_memory <= var.ecs_task_memory
+    error_message = "The ECS_SERVER_CONTAINER_MEMORY + ECS_METRICS_SCRAPER_CONTAINER_MEMORY must be less than or equal to the ECS_TASK_MEMORY"
+  }
+  metrics_scraper_memory_res_validation = {
+    condition     = var.ecs_metrics_scraper_container_memory > var.ecs_metrics_scraper_container_memory_reservation
     error_message = "ECS_METRICS_SCRAPER_CONTAINER_MEMORY_RESERVATION must be less than the ECS_METRICS_SCRAPER_CONTAINER_MEMORY"
   }
 }
