6746abfefe95d1b6189cf65b0fd0e56ed93c3651
Nick Burgan <nick.burgan@exygy.com>
2024-03-14 16:11:46 -0700

    Add warning

--- cloud/shared/bin/rotate_app_secret.py
+++ cloud/shared/bin/rotate_app_secret.py
@@ -1,5 +1,6 @@
 import importlib
 import os
+import sys
 from typing import List
 
 from cloud.shared.bin.lib.config_loader import ConfigLoader
@@ -12,7 +13,14 @@ def run(config: ConfigLoader, _params: List[str]):
     if os.path.exists(rotate_file_py):
         rotate_module = importlib.import_module(
             f'cloud.{config.get_cloud_provider()}.bin.rotate_app_secret')
-        rotate_module.run(config)
+        if sys.stdin.isatty():
+            answer = input(
+                "WARNING: Rotating the app secret will invalidate all existing sessions. Any guest users with an unsubmitted application in progress will lose their application, and logged in users and admins will need to log in again. Are you sure you want to continue? [Y/n]: "
+            )
+            if answer.lower() in ['y', 'yes', '']:
+                rotate_module.run(config)
+            else:
+                exit('Aborting app secret rotation.')
     else:
         exit(
             f'Could not find rotate_app_secret.py for {config.get_cloud_provider()}'
