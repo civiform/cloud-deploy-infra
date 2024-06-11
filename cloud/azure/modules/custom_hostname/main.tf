b9bf18c12592e003861ed94a8ea84b23450ee517
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-06-11 17:04:52 +0000

    remove

--- cloud/azure/modules/custom_hostname/main.tf
+++ cloud/azure/modules/custom_hostname/main.tf
@@ -1,27 +1,3 @@
-data "azurerm_dns_zone" "dns" {
-  name                = var.custom_hostname
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_dns_txt_record" "txt" {
-  name                = "asuid.${azurerm_dns_zone.dns.name}"
-  zone_name           = data.azurerm_dns_zone.dns.name
-  resource_group_name = data.azurerm_dns_zone.dns.resource_group_name
-  ttl                 = 300
-  record {
-    value = var.custom_domain_verification_id
-  }
-}
-
-resource "azurerm_app_service_custom_hostname_binding" "custom_domain_binding" {
-  hostname            = var.custom_hostname
-  app_service_name    = var.app_service_name
-  resource_group_name = var.resource_group_name
-  depends_on          = [azurerm_dns_txt_record.txt]
-  lifecycle {
-    ignore_changes = [ssl_state, thumbprint]
-  }
-}
 
 resource "azurerm_app_service_managed_certificate" "cert" {
   custom_hostname_binding_id = azurerm_app_service_custom_hostname_binding.custom_domain_binding.id
