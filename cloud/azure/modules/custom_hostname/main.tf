03ba8f6ba6fe9e5b0b1cde76aaf63f5555d2509b
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-06-11 16:01:50 +0000

    add certificate

--- cloud/azure/modules/custom_hostname/main.tf
+++ cloud/azure/modules/custom_hostname/main.tf
@@ -1,7 +1,34 @@
+data "azurerm_dns_zone" "dns" {
+  name                = var.custom_hostname
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_dns_cname_record" "cname" {
+  name                = "www"
+  zone_name           = data.azurerm_dns_zone.dns.name
+  resource_group_name = data.azurerm_dns_zone.dns.resource_group_name
+  ttl                 = 300
+  record              = azurerm_app_service.civiform_app.name
+}
+
+resource "azurerm_dns_txt_record" "txt" {
+  name                = "asuid.${azurerm_dns_cname_record.cname.name}"
+  zone_name           = data.azurerm_dns_zone.dns.name
+  resource_group_name = data.azurerm_dns_zone.dns.resource_group_name
+  ttl                 = 300
+  record {
+    value = azurerm_app_service.civiform_app.custom_domain_verification_id
+  }
+}
+
 resource "azurerm_app_service_custom_hostname_binding" "custom_domain_binding" {
   hostname            = var.custom_hostname
   app_service_name    = var.app_service_name
   resource_group_name = var.resource_group_name
+  depends_on          = [azurerm_dns_txt_record.txt]
+  lifecycle {
+    ignore_changes = [ssl_state, thumbprint]
+  }
 }
 
 resource "azurerm_app_service_managed_certificate" "cert" {
