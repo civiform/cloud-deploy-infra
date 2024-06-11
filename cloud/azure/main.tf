d35a16329ef980a32913e12e1e9f025e9b79ef30
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-06-11 18:35:12 +0000

    add back azurem details

--- cloud/azure/main.tf
+++ cloud/azure/main.tf
@@ -5,7 +5,11 @@ terraform {
       source  = "azurerm"
       version = ">=2.65"
     }
+    random = {}
   }
   backend "azurerm" {}
   required_version = ">= 0.14.9"
+}
+provider "azurerm" {
+  features {}
 }
