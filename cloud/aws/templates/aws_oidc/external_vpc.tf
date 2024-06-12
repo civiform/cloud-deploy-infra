341db3b3413352e7b49eba88fb18a11aa41789c9
dkatzz <86739416+dkatzz@users.noreply.github.com>
2024-06-12 11:23:08 -0400

    Update references to external_vpc

--- cloud/aws/templates/aws_oidc/external_vpc.tf
+++ cloud/aws/templates/aws_oidc/external_vpc.tf
@@ -5,20 +5,20 @@
 
 data "aws_vpc" "external" {
   count = local.enable_managed_vpc ? 0 : 1
-  id    = var.external_vpc.id
+  id    = var.external_vpc_id
 }
 
 data "aws_db_subnet_group" "external" {
   count = local.enable_managed_vpc ? 0 : 1
-  name  = var.external_vpc.database_subnet_group_name
+  name  = var.external_vpc_database_subnet_group_name
 }
 
 data "aws_subnet" "external_private" {
   count = local.enable_managed_vpc ? 0 : 1
-  id    = var.external_vpc.private_subnet_id
+  id    = var.external_vpc_private_subnet_id
 }
 
 data "aws_subnet" "external_public" {
   count = local.enable_managed_vpc ? 0 : 1
-  id    = var.external_vpc.public_subnet_id
+  id    = var.external_vpc_public_subnet_id
 }
