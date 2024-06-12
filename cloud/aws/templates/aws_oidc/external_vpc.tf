dac3af04af91e934290527d701055606e8157292
dkatzz <86739416+dkatzz@users.noreply.github.com>
2024-06-12 11:45:10 -0400

    Remove legacy external_vpc references (#336)

--- cloud/aws/templates/aws_oidc/external_vpc.tf
+++ cloud/aws/templates/aws_oidc/external_vpc.tf
@@ -1,24 +1,24 @@
 // File containing the necessary data sources if local.enable_managed_vpc=false.
 //
 // The "local.enable_managed_vpc" variable will be set to false if all of
-// "var.external_vpc" fields are set.
+// "external_vpc" variables are set.
 
 data "aws_vpc" "external" {
   count = local.enable_managed_vpc ? 0 : 1
-  id    = var.external_vpc.id
+  id    = var.external_vpc_id
 }
 
 data "aws_db_subnet_group" "external" {
   count = local.enable_managed_vpc ? 0 : 1
-  name  = var.external_vpc.database_subnet_group_name
+  name  = var.external_vpc_database_subnet_group_name
 }
 
 data "aws_subnet" "external_private" {
   count = local.enable_managed_vpc ? 0 : 1
-  id    = var.external_vpc.private_subnet_id
+  id    = var.external_vpc_private_subnet_id
 }
 
 data "aws_subnet" "external_public" {
   count = local.enable_managed_vpc ? 0 : 1
-  id    = var.external_vpc.public_subnet_id
+  id    = var.external_vpc_public_subnet_id
 }
