a1a8afaf273aa7bd295144e393e270c0fa24872a
dkatzz <86739416+dkatzz@users.noreply.github.com>
2024-06-13 11:55:54 -0400

    Update to use subnet ids

--- cloud/aws/templates/aws_oidc/app.tf
+++ cloud/aws/templates/aws_oidc/app.tf
@@ -323,7 +323,7 @@ module "ecs_fargate_service" {
   ecs_cluster_name          = module.ecs_cluster.aws_ecs_cluster_cluster_name
   ecs_cluster_arn           = module.ecs_cluster.aws_ecs_cluster_cluster_arn
   private_subnets           = local.vpc_private_subnets
-  public_subnets            = local.vpc_public_subnets
+  public_subnets            = local.vpc_public_subnet_ids
   max_cpu_threshold         = var.ecs_max_cpu_threshold
   min_cpu_threshold         = var.ecs_min_cpu_threshold
   max_cpu_evaluation_period = var.ecs_max_cpu_evaluation_period
