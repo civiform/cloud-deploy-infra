e5693b5558a7711e12a065e61bb51ae8f851f294
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-06-14 15:01:34 +0000

    create security group

--- cloud/azure/modules/app/main.tf
+++ cloud/azure/modules/app/main.tf
@@ -250,9 +250,33 @@ resource "azurerm_subnet" "postgres_subnet" {
   }
 }
 
+resource "azurerm_network_security_group" "main" {
+  name                = "${random_pet.server.id}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  subnet_id                 = azurerm_subnet.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
 resource "azurerm_private_dns_zone" "privatelink" {
   name                = "privatelink.postgres.database.azure.com"
   resource_group_name = data.azurerm_resource_group.rg.name
+  depends_on = [azurerm_subnet_network_security_group_association.main]
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
