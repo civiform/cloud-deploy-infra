a4d36f745041424a1fa8c43ec9ff25d88121ff16
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-06-17 16:59:22 +0000

    fix name

--- cloud/azure/modules/app/locals.tf
+++ cloud/azure/modules/app/locals.tf
@@ -18,7 +18,7 @@ locals {
 
     DOCKER_REGISTRY_SERVER_URL = "https://index.docker.io"
 
-    DB_USERNAME    = "${azurerm_postgresql_flexible_server.civiform.administrator_login}@${azurerm_postgresql_flexible_server.civiform.name}"
+    DB_USERNAME    = "${azurerm_postgresql_server.civiform.administrator_login}@${azurerm_postgresql_server.civiform.name}"
     DB_PASSWORD    = data.azurerm_key_vault_secret.postgres_password.value
     DB_JDBC_STRING = "jdbc:postgresql://${local.postgres_private_link}:5432/postgres?ssl=true&sslmode=require"
 
