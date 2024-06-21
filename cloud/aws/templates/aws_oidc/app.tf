f0162078dcc5ce712fbbdd1e010c9eeed43863fc
dkatzz <86739416+dkatzz@users.noreply.github.com>
2024-06-21 13:01:54 -0400

    remove for_each usage
    │ Error: Invalid for_each argument
    │ 
    │   on app.tf line 330, in resource "aws_lb_target_group_attachment" "nlb_tg_attachment":
    │  330:   for_each = toset(data.aws_instances.alb_instances.ids)
    │     ├────────────────
    │     │ data.aws_instances.alb_instances.ids is a list of string, known only after apply
    │ 
    │ The "for_each" set includes values derived from resource attributes that
    │ cannot be determined until apply, and so Terraform cannot determine the
    │ full set of keys that will identify the instances of this resource.
    │ 
    │ When working with unknown values in for_each, it's better to use a map
    │ value where the keys are defined statically in your configuration and where
    │ only the values contain apply-time results.
    │ 
    │ Alternatively, you could use the -target planning option to first apply
    │ only the resources that the for_each value depends on, and then apply a
    │ second time to fully converge.

--- cloud/aws/templates/aws_oidc/app.tf
+++ cloud/aws/templates/aws_oidc/app.tf
@@ -324,13 +324,17 @@ data "aws_instances" "alb_instances" {
   }
 }
 
+locals {
+  instance_count = length(data.aws_instances.alb_instances.ids)
+}
+
 # Attach ALB instances to the target group (one attachment per instance)
 resource "aws_lb_target_group_attachment" "nlb_tg_attachment" {
   # Assuming the ALB is in a private subnet and can only be accessed by the NLB, the NLB's target group should be defined as follows:
-  for_each = toset(data.aws_instances.alb_instances.ids)
+   count = local.instance_count
 
   target_group_arn = module.ecs_fargate_service.alb_target_group_arn
-  target_id        = each.key
+  target_id        = data.aws_instances.alb_instances.ids[count.index]
   port             = var.port
 
   depends_on = [module.ecs_fargate_service]  
