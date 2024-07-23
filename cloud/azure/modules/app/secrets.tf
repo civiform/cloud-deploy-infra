9cafa58cabec2100f1dbb9c86b0969b3df49d6cf
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-07-23 15:45:55 +0000

    remove location

--- cloud/azure/modules/app/secrets.tf
+++ cloud/azure/modules/app/secrets.tf
@@ -1,7 +1,6 @@
 data "azurerm_key_vault" "civiform_key_vault" {
   name                = var.key_vault_name
   resource_group_name = data.azurerm_resource_group.rg.name
-  location = data.azurerm_resource_group.rg.location
 }
 
 data "azurerm_key_vault_secret" "postgres_password" {
