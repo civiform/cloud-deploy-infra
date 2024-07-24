69784bfd81e7f9b6f3af6b2d7ae4c63c29f8a2b8
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-07-24 16:52:23 +0000

    finish

--- cloud/azure/bin/lib/key_vault.sh
+++ cloud/azure/bin/lib/key_vault.sh
@@ -127,5 +127,5 @@ function key_vault::has_secret() {
     -o tsv 2>&1 >/dev/null)"
   echo "SECRET #####"
   echo ${SECRET_RESULT}
-  [[ $SECRET_RESULT =~ "SecretNotFound" ]] && echo "False" || echo "True" 
+  [[ $SECRET_RESULT =~ "SecretNotFound" ]] && echo 0 || echo 1
 }
