8b8d2a1e2722951dca5a0db4a1a378ccad0ade66
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-07-26 18:19:05 +0000

    commit

--- cloud/azure/modules/app/main.tf
+++ cloud/azure/modules/app/main.tf
@@ -100,7 +100,7 @@ resource "azurerm_linux_web_app" "civiform_app" {
     application_stack {
       # docker_registry_url = "https://docker.io"
       # docker_image_name   = "DOCKER|civiform/civiform"
-      docker_image_tag = var.image_tag
+      docker_image_tag = "${var.image_tag}"
       docker_image     = "civiform/civiform"
     }
   }
