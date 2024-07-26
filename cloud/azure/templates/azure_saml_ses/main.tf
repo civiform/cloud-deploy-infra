dc3e218a78bc621b7bc45ad42bce99f57b4edb15
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-07-26 17:40:25 +0000

    email service

--- cloud/azure/templates/azure_saml_ses/main.tf
+++ cloud/azure/templates/azure_saml_ses/main.tf
@@ -79,13 +79,13 @@ module "saml_keystore" {
   resource_group_name          = var.azure_resource_group
 }
 
-# module "email_service" {
-#   for_each = toset([
-#     var.sender_email_address,
-#     var.staging_applicant_notification_mailing_list,
-#     var.staging_ti_notification_mailing_list,
-#     var.staging_program_admin_notification_mailing_list
-#   ])
-#   source               = "../../../aws/modules/ses"
-#   sender_email_address = each.key
-# }
+module "email_service" {
+  for_each = toset([
+    var.sender_email_address,
+    var.staging_applicant_notification_mailing_list,
+    var.staging_ti_notification_mailing_list,
+    var.staging_program_admin_notification_mailing_list
+  ])
+  source               = "../../../aws/modules/ses"
+  sender_email_address = each.key
+}
