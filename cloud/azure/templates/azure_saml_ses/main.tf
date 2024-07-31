5b7c6d155beae741089ffee27fc0053ef7787d3a
jeffpw-goog <165937506+jeffpw-goog@users.noreply.github.com>
2024-07-31 14:58:32 -0600

    Remove dead feature flags from azure terraform templates (#359)
    rebased

--- cloud/azure/templates/azure_saml_ses/main.tf
+++ cloud/azure/templates/azure_saml_ses/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.61.0"
+      version = "5.15.0"
     }
     azurerm = {
       source  = "azurerm"
-      version = "3.114.0"
+      version = "3.70.0"
     }
     random = {}
   }
@@ -21,8 +21,9 @@ module "app" {
 
   postgres_admin_login = var.postgres_admin_login
 
-  postgres_sku_name = var.postgres_sku_name
-  postgres_storage_mb = var.postgres_storage_mb
+  # note that we must use GP tier
+  postgres_sku_name = "GP_Gen5_2"
+
   image_tag = var.image_tag
 
   civiform_applicant_auth_protocol = var.civiform_applicant_auth_protocol
@@ -55,8 +56,11 @@ module "app" {
   saml_keystore_storage_account_name   = module.saml_keystore.storage_account_name
   saml_keystore_storage_container_name = module.saml_keystore.storage_container_name
 
-  # civiform_api_keys_ban_global_subnet = var.civiform_api_keys_ban_global_subnet
-  # civiform_server_metrics_enabled     = var.civiform_server_metrics_enabled
+  #feature_flag_reporting_enabled       = var.feature_flag_reporting_enabled
+  #feature_flag_status_tracking_enabled = var.feature_flag_status_tracking_enabled
+  #civiform_api_keys_ban_global_subnet  = var.civiform_api_keys_ban_global_subnet
+ # civiform_server_metrics_enabled      = var.civiform_server_metrics_enabled
+  #feature_flag_overrides_enabled       = var.feature_flag_overrides_enabled
 }
 
 module "custom_hostname" {
@@ -77,13 +81,13 @@ module "saml_keystore" {
   resource_group_name          = var.azure_resource_group
 }
 
-# module "email_service" {
-#   for_each = toset([
-#     var.sender_email_address,
-#     var.staging_applicant_notification_mailing_list,
-#     var.staging_ti_notification_mailing_list,
-#     var.staging_program_admin_notification_mailing_list
-#   ])
-#   source               = "../../../aws/modules/ses"
-#   sender_email_address = each.key
-# }
+module "email_service" {
+  for_each = toset([
+    var.sender_email_address,
+    var.staging_applicant_notification_mailing_list,
+    var.staging_ti_notification_mailing_list,
+    var.staging_program_admin_notification_mailing_list
+  ])
+  source               = "../../../aws/modules/ses"
+  sender_email_address = each.key
+}
