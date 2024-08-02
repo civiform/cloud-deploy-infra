e2535e993d418b41ee38aec4ae7ea45120bf88ae
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-08-02 18:35:02 +0000

    change hostname

--- cloud/azure/modules/app/outputs.tf
+++ cloud/azure/modules/app/outputs.tf
@@ -1,5 +1,5 @@
 output "app_service_default_hostname" {
-  value = "https://${azurerm_linux_web_app.civiform_app.default_site_hostname}"
+  value = "https://${azurerm_linux_web_app.civiform_app.name}.azurewebsites.net"
 }
 
 # output "app_service_canary_hostname" {
