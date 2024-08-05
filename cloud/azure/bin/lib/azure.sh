adeb2159af91c49a3dd1fa3d4342bc410667f116
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-08-05 20:47:34 +0000

    change to primary

--- cloud/azure/bin/lib/azure.sh
+++ cloud/azure/bin/lib/azure.sh
@@ -144,7 +144,7 @@ function azure::set_new_container_tag() {
   az webapp config container set \
     --resource-group "${1}" \
     --name "${2}" \
-    --slot "canary" \
+    --slot "primary" \
     --container-image-name "DOCKER|civiform/civiform:${3}"
 }
 
