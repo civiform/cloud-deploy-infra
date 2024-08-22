0e56b2ceb90af58488a4aff2377366120fc0faaa
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-08-22 17:51:45 +0000

    private

--- cloud/azure/modules/app/main.tf
+++ cloud/azure/modules/app/main.tf
@@ -172,7 +172,7 @@ resource "azurerm_postgresql_flexible_server" "civiform" {
   version                       = "15"
   storage_mb                    = var.postgres_storage_mb
   public_network_access_enabled = false
-  depends_on             = [azurerm_private_endpoint.endpoint]
+  depends_on                    = [azurerm_private_dns_zone_virtual_network_link.virtual]
   lifecycle {
     ignore_changes = [
       zone
