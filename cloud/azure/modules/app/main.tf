34f93022285f3e0f4f9d4222afcbe20542bee229
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-08-23 15:04:49 +0000

    commit

--- cloud/azure/modules/app/main.tf
+++ cloud/azure/modules/app/main.tf
@@ -161,7 +161,7 @@ resource "azurerm_postgresql_flexible_server" "civiform" {
   version                       = "15"
   storage_mb                    = var.postgres_storage_mb
   # public_network_access_enabled = false
-  private_dns_zone_id           = azurerm_private_dns_zone.privatedns.id
+  # private_dns_zone_id           = azurerm_private_dns_zone.privatedns.id
   # delegated_subnet_id           = azurerm_subnet.postgres_subnet.id
   # depends_on                    = [azurerm_private_dns_zone_virtual_network_link.networklink]
 
