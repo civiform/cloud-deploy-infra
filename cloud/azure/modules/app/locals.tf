9ab240d6690f75ffd3c1a0a5b7ccd6496c146101
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-08-27 20:25:53 +0000

    local

--- cloud/azure/modules/app/locals.tf
+++ cloud/azure/modules/app/locals.tf
@@ -25,7 +25,7 @@ locals {
     DB_USERNAME = "${azurerm_postgresql_flexible_server.civiform.administrator_login}@${azurerm_postgresql_flexible_server.civiform.name}"
     DB_PASSWORD = data.azurerm_key_vault_secret.postgres_password.value
     #DB_JDBC_STRING       = "jdbc:postgresql://${azurerm_postgresql_flexible_server.civiform.name}.postgres.database.azure.com:5432/postgres?user=${azurerm_postgresql_flexible_server.civiform.administrator_login}&password=${azurerm_postgresql_flexible_server.civiform.administrator_password}&sslmode=require"
-    DB_JDBC_STRING = "jdbc:postgresql://${locals.postgres_private_link}:5432/postgres?user=${azurerm_postgresql_flexible_server.civiform.administrator_login}&password=${azurerm_postgresql_flexible_server.civiform.administrator_password}&sslmode=require"
+    DB_JDBC_STRING = "jdbc:postgresql://${local.postgres_private_link}:5432/postgres?user=${azurerm_postgresql_flexible_server.civiform.administrator_login}&password=${azurerm_postgresql_flexible_server.civiform.administrator_password}&sslmode=require"
 
     STORAGE_SERVICE_NAME = "azure-blob"
 
