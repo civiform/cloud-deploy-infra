2c0d35b28980322fd355b403222c874305123c33
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-09-05 16:57:22 +0000

    commit

--- cloud/azure/modules/app/role-assignments.tf
+++ cloud/azure/modules/app/role-assignments.tf
@@ -6,10 +6,16 @@ resource "azurerm_role_assignment" "storage_blob_delegator" {
   principal_id         = azurerm_linux_web_app.civiform_app.identity.0.principal_id
 }
 
-resource "azurerm_role_assignment" "key_vault_secrets_user" {
+resource "azurerm_role_assignment" "key_vault_secrets_principal_user" {
   scope                = data.azurerm_key_vault.civiform_key_vault.id
   role_definition_name = "Key Vault Secrets User"
   principal_id         = azurerm_linux_web_app.civiform_app.identity.0.principal_id
+}
+
+resource "azurerm_role_assignment" "key_vault_secrets_staging_user" {
+  scope                = data.azurerm_key_vault.civiform_key_vault.id
+  role_definition_name = "Key Vault Secrets User for staging deployment "
+  principal_id         = "f552a386-fd60-40bf-93a7-57c441bb0c99"
 }
 
 resource "azurerm_role_assignment" "storage_blob_data_contributor" {
