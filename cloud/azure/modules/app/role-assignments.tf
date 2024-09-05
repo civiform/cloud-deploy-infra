b4ff52ffdeb48a58dd2067f742458bfc506e18f9
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-09-05 20:00:52 +0000

    remove

--- cloud/azure/modules/app/role-assignments.tf
+++ cloud/azure/modules/app/role-assignments.tf
@@ -12,12 +12,6 @@ resource "azurerm_role_assignment" "key_vault_secrets_principal_user" {
   principal_id         = azurerm_linux_web_app.civiform_app.identity.0.principal_id
 }
 
-resource "azurerm_role_assignment" "key_vault_secrets_key_vault_reference_user" {
-  scope                = data.azurerm_key_vault.civiform_key_vault.id
-  role_definition_name = "Key Vault Administrator"
-  principal_id         = azurerm_linux_web_app.civiform_app.key_vault_reference_identity_id
-}
-
 resource "azurerm_role_assignment" "storage_blob_data_contributor" {
   scope                = azurerm_storage_account.files_storage_account.id
   role_definition_name = "Storage Blob Data Contributor"
