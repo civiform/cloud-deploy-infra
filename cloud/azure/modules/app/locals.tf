2f5f51c0db132302dadbd6c5400001593c903b16
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-09-13 15:21:47 -0400

    Minor v4.1.0 azurerm bugfixes (#387)

--- cloud/azure/modules/app/locals.tf
+++ cloud/azure/modules/app/locals.tf
@@ -22,11 +22,6 @@ locals {
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
     PORT                                = 9000
 
-    DB_USERNAME = "${azurerm_postgresql_flexible_server.civiform.administrator_login}@${azurerm_postgresql_flexible_server.civiform.name}"
-    DB_PASSWORD = data.azurerm_key_vault_secret.postgres_password.value
-    #DB_JDBC_STRING       = "jdbc:postgresql://${azurerm_postgresql_flexible_server.civiform.name}.postgres.database.azure.com:5432/postgres?user=${azurerm_postgresql_flexible_server.civiform.administrator_login}&password=${azurerm_postgresql_flexible_server.civiform.administrator_password}&sslmode=require"
-    DB_JDBC_STRING = "jdbc:postgresql://notable-killdeer-civiform.postgres.database.azure.com:5432/postgres?user=psqladmin&password=${azurerm_postgresql_flexible_server.civiform.administrator_password}&sslmode=require"
-
     STORAGE_SERVICE_NAME = "azure-blob"
 
     AZURE_STORAGE_ACCOUNT_NAME      = azurerm_storage_account.files_storage_account.name
