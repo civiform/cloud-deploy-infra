96822865f51b7001a7a2f72e6872554d4d223501
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-09-17 20:32:54 +0000

    add aws

--- cloud/azure/templates/azure_saml_ses/providers.tf
+++ cloud/azure/templates/azure_saml_ses/providers.tf
@@ -3,3 +3,7 @@ provider "azurerm" {
   # https://github.com/civiform/civiform/issues/8598
   subscription_id = "4ef4ae1b-c966-4ac4-9b7c-a837ea410821"
 }
+
+provider "aws" {
+  region = var.aws_region
+}
