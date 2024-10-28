46fa72eade0c17195463311d5ca40ef9110a62e1
Ricardo Lagos <lagosr@google.com>
2024-10-28 09:49:17 -0700

    Add ability to skip provider registrations for the terraform azure provider. (#408)

--- cloud/azure/templates/azure_saml_ses/providers.tf
+++ cloud/azure/templates/azure_saml_ses/providers.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   features {}
   # https://github.com/civiform/civiform/issues/8598
-  subscription_id = "4ef4ae1b-c966-4ac4-9b7c-a837ea410821"
+  subscription_id            = "4ef4ae1b-c966-4ac4-9b7c-a837ea410821"
+  skip_provider_registration = var.azure_skip_provider_registration
 }
