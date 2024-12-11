d41a2880b3a54fb9c747f9906b35d4d17898b7e8
Ricardo Lagos <lagosr@google.com>
2024-12-11 22:50:46 +0000

    reference the environment variable for azure_subscription in the provider terraform

--- cloud/azure/templates/azure_saml_ses/providers.tf
+++ cloud/azure/templates/azure_saml_ses/providers.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   features {}
   # https://github.com/civiform/civiform/issues/8598
+  subscription_id            = var.azure_subscription
   skip_provider_registration = var.azure_skip_provider_registration
 }
