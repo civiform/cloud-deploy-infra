88669e7a56dc315c88b2819248de2939d5637e79
bion <bionjohnson@gmail.com>
2024-12-30 22:13:39 -0800

    Python formatter

--- cloud/gcp/templates/saas/cli/tenant.py
+++ cloud/gcp/templates/saas/cli/tenant.py
@@ -20,16 +20,11 @@ class Tenant():
     env_var_secrets_name: str = None
     static_ip_name: str = None
 
-    props = ["cluster_location",
-             "cluster_name",
-             "image",
-             "image_tag",
-             "project_id",
-             "region",
-             "ssl_cert_name",
-             "tenant_id",
-             "tf_state_bucket_name",
-             "tf_state_tenant_prefix"]
+    props = [
+        "cluster_location", "cluster_name", "image", "image_tag", "project_id",
+        "region", "ssl_cert_name", "tenant_id", "tf_state_bucket_name",
+        "tf_state_tenant_prefix"
+    ]
 
     def __init__(self, tenant_config_filename):
         self.tenant_config_filename = tenant_config_filename
@@ -72,7 +67,8 @@ class Tenant():
         self._helm_delete()
 
         print(
-            f"Deleting remaining namespace {self.tenant_namespace} resources, if any")
+            f"Deleting remaining namespace {self.tenant_namespace} resources, if any"
+        )
         shell(f"kubectl delete all -n {self.tenant_namespace} --all")
         print(f"Deleting namespace {self.tenant_namespace}")
         shell(f"kubectl delete namespace {self.tenant_namespace}")
@@ -88,11 +84,13 @@ class Tenant():
 
     def _create_static_ip(self):
         shell(
-            f"gcloud compute addresses create {self.static_ip_name} --global --quiet")
+            f"gcloud compute addresses create {self.static_ip_name} --global --quiet"
+        )
 
     def _delete_static_ip(self):
         shell(
-            f"gcloud compute addresses delete {self.static_ip_name} --global --quiet")
+            f"gcloud compute addresses delete {self.static_ip_name} --global --quiet"
+        )
 
     def _tf_destroy(self, tf_vars_file):
         tofu_path = os.path.abspath("data_plane/tofu")
@@ -103,26 +101,35 @@ class Tenant():
         # than altering the internal DB permissions.
         # shell(f"tofu -chdir={tofu_path} state rm google_sql_user.civiform_user")
         shell(
-            f"tofu -chdir={tofu_path} destroy -input=false -var-file={tf_vars_file.name}")
+            f"tofu -chdir={tofu_path} destroy -input=false -var-file={tf_vars_file.name}"
+        )
 
     def _tf_init_and_apply(self, tf_vars_file):
         tofu_path = os.path.abspath("data_plane/tofu")
         backend_config = f"-backend-config='bucket={self.tenant_config['tf_state_bucket_name']}' -backend-config='prefix={self.tenant_config['tf_state_tenant_prefix']}'"
 
         shell(
-            f"tofu -chdir={tofu_path} init -reconfigure -input=false {backend_config}")
+            f"tofu -chdir={tofu_path} init -reconfigure -input=false {backend_config}"
+        )
         shell(
-            f"tofu -chdir={tofu_path} apply -auto-approve -input=false -var-file={tf_vars_file.name} -var='db_enable_public_ip4=true'")
+            f"tofu -chdir={tofu_path} apply -auto-approve -input=false -var-file={tf_vars_file.name} -var='db_enable_public_ip4=true'"
+        )
 
     def _tf_write_vars(self):
         # TODO: use canonical var definitions similar to the AWS deployment for filtering and validating vars
-        tf_vars = {k: self.tenant_config[k] for k in self.props if k not in [
-            "image", "image_tag", "tf_state_bucket_name", "tf_state_tenant_prefix"]}
+        tf_vars = {
+            k: self.tenant_config[k] for k in self.props if k not in [
+                "image", "image_tag", "tf_state_bucket_name",
+                "tf_state_tenant_prefix"
+            ]
+        }
         tf_vars['tenant_ksa_name'] = self.ksa_name
         tf_vars['tenant_k8s_namespace'] = f"ns-tenant-{self.tenant_id}"
-        tf_vars['db_deletion_protection'] = self.tenant_config['db_deletion_protection'] if 'db_deletion_protection' in self.tenant_config else False
+        tf_vars['db_deletion_protection'] = self.tenant_config[
+            'db_deletion_protection'] if 'db_deletion_protection' in self.tenant_config else False
 
-        with tempfile.NamedTemporaryFile(mode="w+", suffix=".tfvars.json", delete=False) as temp_vars_file:
+        with tempfile.NamedTemporaryFile(mode="w+", suffix=".tfvars.json",
+                                         delete=False) as temp_vars_file:
             temp_vars_file.write(json.dumps(tf_vars, sort_keys=True, indent=2))
             temp_vars_file.write("\n")
             temp_vars_file.close()
@@ -141,53 +148,73 @@ class Tenant():
     #       and remove cloud-sql-proxy from civ.Dockerfile
     def _db_do_service_account_grants(self):
         shell(
-            f"./data_plane/service_account_db_grants {self.db_conn} {self.db_username}")
+            f"./data_plane/service_account_db_grants {self.db_conn} {self.db_username}"
+        )
 
     def _kubectl_create_random_secrets(self):
         shell(
-            f"kubectl create secret generic server-secret-key --from-literal='SECRET_KEY={generate_random_string(64)}' --namespace {self.tenant_namespace}")
+            f"kubectl create secret generic server-secret-key --from-literal='SECRET_KEY={generate_random_string(64)}' --namespace {self.tenant_namespace}"
+        )
         shell(
-            f"kubectl create secret generic server-api-secret-salt --from-literal='CIVIFORM_API_SECRET_SALT={generate_random_string(16)}' --namespace {self.tenant_namespace}")
+            f"kubectl create secret generic server-api-secret-salt --from-literal='CIVIFORM_API_SECRET_SALT={generate_random_string(16)}' --namespace {self.tenant_namespace}"
+        )
 
     def _kubectl_apply_server_env_vars(self):
-        temp = self._write_tempfile(".yaml", yaml.dump({
-            'apiVersion': 'v1',
-            'kind': 'ConfigMap',
-            'metadata': {'namespace': self.tenant_namespace, 'name': self.env_var_config_map_name},
-            'data': self.tenant_config['server_env_vars']
-        }))
+        temp = self._write_tempfile(
+            ".yaml",
+            yaml.dump(
+                {
+                    'apiVersion': 'v1',
+                    'kind': 'ConfigMap',
+                    'metadata':
+                        {
+                            'namespace': self.tenant_namespace,
+                            'name': self.env_var_config_map_name
+                        },
+                    'data': self.tenant_config['server_env_vars']
+                }))
 
         shell(f"kubectl apply -f {temp.name}")
 
     def _kubectl_apply_server_secret_env_vars(self):
-        temp = self._write_tempfile(".yaml", yaml.dump({
-            'apiVersion': 'v1',
-            'kind': 'Secret',
-            'metadata': {'namespace': self.tenant_namespace, 'name': self.env_var_secrets_name},
-            'stringData': self.tenant_config['server_secret_env_vars']
-        }))
+        temp = self._write_tempfile(
+            ".yaml",
+            yaml.dump(
+                {
+                    'apiVersion': 'v1',
+                    'kind': 'Secret',
+                    'metadata':
+                        {
+                            'namespace': self.tenant_namespace,
+                            'name': self.env_var_secrets_name
+                        },
+                    'stringData': self.tenant_config['server_secret_env_vars']
+                }))
 
         shell(f"kubectl apply -f {temp.name}")
         # don't leave secrets sitting around on the filesystem
         os.remove(temp.name)
 
     def _helm_write_values(self):
-        return self._write_tempfile(".yaml", yaml.dump({
-            'dbConnectionName': self.db_conn,
-            'dbUsername': self.db_username,
-            'gsaEmail': self.gsa_email,
-            'ksaName': self.ksa_name,
-            'image': self.tenant_config['image'],
-            'imageTag': self.tenant_config['image_tag'],
-            'nodePoolName': f"np-tenant-{self.tenant_id}",
-            'publicFQDN': f"{self.tenant_id}.tenant.civiform.dev",
-            'sslCertName': self.tenant_config['ssl_cert_name'],
-            'tenantId': self.tenant_id,
-            'namespace': self.tenant_namespace,
-            'envVarConfigMapName': self.env_var_config_map_name,
-            'envVarSecretName': self.env_var_secrets_name,
-            'staticIPName': self.static_ip_name
-        }))
+        return self._write_tempfile(
+            ".yaml",
+            yaml.dump(
+                {
+                    'dbConnectionName': self.db_conn,
+                    'dbUsername': self.db_username,
+                    'gsaEmail': self.gsa_email,
+                    'ksaName': self.ksa_name,
+                    'image': self.tenant_config['image'],
+                    'imageTag': self.tenant_config['image_tag'],
+                    'nodePoolName': f"np-tenant-{self.tenant_id}",
+                    'publicFQDN': f"{self.tenant_id}.tenant.civiform.dev",
+                    'sslCertName': self.tenant_config['ssl_cert_name'],
+                    'tenantId': self.tenant_id,
+                    'namespace': self.tenant_namespace,
+                    'envVarConfigMapName': self.env_var_config_map_name,
+                    'envVarSecretName': self.env_var_secrets_name,
+                    'staticIPName': self.static_ip_name
+                }))
 
     def _write_tempfile(self, suffix, contents):
         temp = tempfile.NamedTemporaryFile("w+", suffix=suffix, delete=False)
@@ -200,15 +227,18 @@ class Tenant():
 
     def _helm_install(self, helm_values_file):
         shell(
-            f"helm install -f {helm_values_file.name} tenant-{self.tenant_id} ./data_plane/kubernetes/tenant_chart")
+            f"helm install -f {helm_values_file.name} tenant-{self.tenant_id} ./data_plane/kubernetes/tenant_chart"
+        )
 
     def _helm_upgrade(self, helm_values_file):
         shell(
-            f"helm upgrade -f {helm_values_file.name} tenant-{self.tenant_id} ./data_plane/kubernetes/tenant_chart")
+            f"helm upgrade -f {helm_values_file.name} tenant-{self.tenant_id} ./data_plane/kubernetes/tenant_chart"
+        )
 
     def _helm_delete(self):
         shell(
-            f"helm delete tenant-{self.tenant_id} ./data_plane/kubernetes/tenant_chart")
+            f"helm delete tenant-{self.tenant_id} ./data_plane/kubernetes/tenant_chart"
+        )
 
     def _load_tenant_config(self):
         try:
@@ -217,7 +247,8 @@ class Tenant():
 
         except FileNotFoundError:
             print(
-                f"Error: tenant_config file not found: {self.tenant_config_filename}")
+                f"Error: tenant_config file not found: {self.tenant_config_filename}"
+            )
             exit(1)
         except yaml.YAMLError as e:
             print(f"Error: Invalid YAML format: {e}")
