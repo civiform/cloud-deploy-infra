8b1bd105cae24fcf4e1061fd419d8521fa6a445a
dkatzz <86739416+dkatzz@users.noreply.github.com>
2025-02-26 23:07:30 +0000

    fmt

--- cloud/azure/modules/app/locals.tf
+++ cloud/azure/modules/app/locals.tf
@@ -56,8 +56,8 @@ locals {
     # azure AD to not include that claim.
     ADFS_ADDITIONAL_SCOPES = ""
   }, var.civiform_server_environment_variables)
-  adfs_client_id     = "adfs-client-id"
-  adfs_discovery_uri = "adfs-discovery-uri"
-  applicant_oidc_client_id = "applicant-oidc-client-id"
+  adfs_client_id               = "adfs-client-id"
+  adfs_discovery_uri           = "adfs-discovery-uri"
+  applicant_oidc_client_id     = "applicant-oidc-client-id"
   applicant_oidc_client_secret = "applicant-oidc-client-secret"
 }
