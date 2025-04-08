3cb7b35a616545c34e4149b3d0718a7aa1ff5ac7
Rocky Fine <rocky.fine@exygy.com>
2025-04-08 11:06:49 -0400

    try to fix name again

--- cloud/azure/modules/app/main.tf
+++ cloud/azure/modules/app/main.tf
@@ -1,7 +1,7 @@
 locals {
   # We use the first 15 characters of the resource group and strip all non-alphanumeric
   # characters to match restrictions on resource naming
-  formatted_resource_group_name = replace(substr(var.resource_group_name, 0, 15), "[^a-zA-Z0-9]", "")
+  formatted_resource_group_name = replace(substr(var.resource_group_name, 0, 9), "[^a-zA-Z0-9]", "")
 }
 
 resource "random_pet" "server" {}
