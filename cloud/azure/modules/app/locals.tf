31d4dbff3639ec6e4c51b03d7e34bc7d0a6f87f6
dkatzz <86739416+dkatzz@users.noreply.github.com>
2025-05-23 16:03:57 +0000

    updates

--- cloud/azure/modules/app/locals.tf
+++ cloud/azure/modules/app/locals.tf
@@ -14,10 +14,8 @@ locals {
   adfs_secret_keyvault_id         = "adfs-secret"
 
   adfs_client_id               = "adfs-client-id"
-  adfs_discovery_uri           = "adfs-discovery-uri"
   applicant_oidc_client_id     = "applicant-oidc-client-id"
   applicant_oidc_client_secret = "applicant-oidc-client-secret"
-  applicant_oidc_discovery_uri = "applicant-oidc-discovery-uri"
 
   app_settings = merge({
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
@@ -39,10 +37,8 @@ locals {
 
     ADFS_SECRET                  = data.azurerm_key_vault_secret.adfs_secret.value
     ADFS_CLIENT_ID               = data.azurerm_key_vault_secret.adfs_client_id.value
-    ADFS_DISCOVERY_URI           = data.azurerm_key_vault_secret.adfs_discovery_uri.value
     APPLICANT_OIDC_CLIENT_SECRET = data.azurerm_key_vault_secret.applicant_oidc_client_secret.value
     APPLICANT_OIDC_CLIENT_ID     = data.azurerm_key_vault_secret.applicant_oidc_client_id.value
-    APPLICANT_OIDC_DISCOVERY_URI = data.azurerm_key_vault_secret.applicant_oidc_discovery_uri.value
 
     # The values below are all defaulted to null. If SAML authentication is used, the values can be pulled from the
     # saml_keystore module
