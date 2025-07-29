8cad29dde4eb7912228c97df96887e1bed0a2712
osajulian-dev <osao@google.com>
2025-07-29 18:51:04 +0000

    feat: route ECS autoscaling CloudWatch alarms to civiform_alert SNS topic (#506)

--- cloud/aws/modules/ecs_fargate_service/main.tf
+++ cloud/aws/modules/ecs_fargate_service/main.tf
@@ -357,6 +357,7 @@ module "ecs-autoscaling" {
   scale_target_max_capacity = var.scale_target_max_capacity
   scale_target_min_capacity = var.scale_target_min_capacity
   tags                      = var.tags
+  sns_topic_arn             = var.sns_topic_arn
 }
 
 moved {
